6ada85286ef73ca07462833128b679286083373a
Prashi <prashivyas05@gmail.com>
2025-12-17 17:45:17 +0530

    updated script

--- App.jsx
+++ App.jsx
@@ -1,41 +1,30 @@
 import React from "react";
-// import { Routes, Route } from "react-router-dom";
+ import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import About from "./Components/About";
 import Blog from "./Components/Blog";
-// import Home from "./Components/Home";
+ import Home from "./Components/Home";
 import Services from "./Components/Services";
 import Questions from "./Components/Questions";
 
 
-// function App() {
-//   return (
-//     <>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/Blog" element={<Blog />} />
-//         <Route path ="/Services" element={<Services/>}/>
-//         <Route path ="/Questions" element={<Questions/>}/>
-//       </Routes>
-//     </>
-//   );
-// }
+ function App() {
+   return (
+     <>
+       <Navbar />
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/about" element={<About />} />
+         <Route path="/Blog" element={<Blog />} />
+         <Route path ="/Services" element={<Services/>}/>
+         <Route path ="/Questions" element={<Questions/>}/>
+       </Routes>
+     </>
+   );
+ }
 
-// export default App;
-import { Routes, Route } from "react-router-dom";
-import Home from "./Components/Home";
+ export default App;
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  );
-}
-
-export default App;
 
 
 
